1ca466d919e53ad640ea3c12a137f42a47f367e4
Mohammad Hussaini <mohammad@Mohammads-MacBook-Pro.local>
2024-04-23 19:06:46 -0400

    [UPDATE] - added an add task button

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
     static displayName = Home.name;
-   
+    
 
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
+            <p>Welcome to your new single-page application, built with:</p>
+            <HTMLButtonElement>Add Task</HTMLButtonElement>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
