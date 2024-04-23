635e811042359e1b7753bd2da9902dc485f309dc
Mohammad Hussaini <mohammad@Mohammads-MacBook-Pro.local>
2024-04-23 18:58:07 -0400

    [UPDATE] - Added an task button

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+   <GamepadButton>Add Task</>
 
   render() {
     return (
@@ -12,6 +13,7 @@ export class Home extends Component {
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
           <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
+          
         </ul>
         <p>To help you get started, we have also set up:</p>
         <ul>
